9b6443ca3c81c2bc84cd917abc06262a51816103
Santiago Baron Zuleta <santiagobazul@hotmail.com>
2021-07-24 21:08:21 -0500

    Version 1.2 - Comments added, bugs fixed, styles changed

--- js/main.js
+++ js/main.js
@@ -2,26 +2,38 @@
 
 window.addEventListener('load',function(){
 
+    // Button and result variables
+
     var button = document.querySelector("#button"),
         result = document.querySelector(".result");
+
+    // Button event to calculate BMI
 
     button.addEventListener("click", function(e){
         e.preventDefault()
 
+        // Input variables
+
         var peso = parseInt(document.querySelector("#peso").value),
             altura = parseInt(document.querySelector("#altura").value);
 
-        const IMC = (peso/Math.pow(altura,2))*10000;
+        // Calculate the BMI if the inputs are filled with numbers
 
-        result.innerHTML = `
-    
-        <p>Tu Indice de masa corporal es: ${IMC}</p>
-        
-        `
+        if(!isNaN(peso) && !isNaN(altura)){
+
+            const IMC = ((peso/Math.pow(altura,2))*10000).toFixed(2);
+            console.log(peso);
+
+            // Show the result
+
+            result.innerHTML = `
+            <p>Tu Indice de masa corporal es: ${IMC}</p>
+            `
+        }else{
+            result.innerHTML = `
+            <p>Debes ingresar todos los datos para calcular tu IMC</p>
+            `
+        }
 
     });
-
-
-
-
 });
